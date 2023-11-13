b64eccda594e6b82d83d98161ee99f230cd378e5
Artemis Hoogsteen <d.hoogsteen@student.rug.nl>
2023-11-13 12:21:48 +0100

    Made figure

--- Script/First_Script.R
+++ Script/First_Script.R
@@ -1,12 +1,18 @@
-# my first github synced script :D
+# my first github synced script :D 
 library(renv)
 # restore the versions of the packages used to develop this project
 renv::restore()
+# 1. pull in github to get up to date version
+library(tidyverse)
+# install.packages("tidyverse")
 
 x <- c(1,2,3,4,5)
 y <- c(4,2,0,6,9)
 data<- data.frame(x,y)
-data
-fig01 <-
-  
+fig01 <- data %>% ggplot(aes(x=x,y=y))+
+  geom_point(col="darkblue",size=5,shape="o")+
+  geom_smooth(method="lm", col="yellow",fill="orange")
+fig01
+ggsave("./Figures/fig01.png", plot=fig01,width=1920, height=1068, units="px")
+
   
