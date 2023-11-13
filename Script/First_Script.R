5b51862f3d0a5f01fe008326df2dbd4f6294fcad
Artemis Hoogsteen <d.hoogsteen@student.rug.nl>
2023-11-13 12:05:37 +0100

    project initialized with renv

--- Script/First_Script.R
+++ Script/First_Script.R
@@ -1,1 +1,12 @@
 # my first github synced script :D
+library(renv)
+# restore the versions of the packages used to develop this project
+renv::restore()
+
+x <- c(1,2,3,4,5)
+y <- c(4,2,0,6,9)
+data<- data.frame(x,y)
+data
+fig01 <-
+  
+  
